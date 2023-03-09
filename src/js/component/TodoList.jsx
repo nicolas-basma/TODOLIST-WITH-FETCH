35a94654dcc08c460659c6c259c117c32a6d3344
NicoB <nicobasma_@hotmail.com>
2023-03-09 19:21:23 +0000

    todoList with fetch

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -5,10 +5,37 @@ const TodoList = () => {
   const [tareas, setTarea] = useState([]);
   const [idForDelete, setIdForDelete] = useState([])
 
+useEffect(()=>{
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/nicoydani')
+  .then((res)=> res.json())
+  .then((res) => setTarea(res))
+},[]);
+
+useEffect(()=>{
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/nicoydani',{ method: "PUT",
+  body: JSON.stringify(tareas),
+  headers: {
+    "Content-Type": "application/json"
+  }})
+  .then(()=> console.log("All is good"))
+  .catch(()=> console.log("uy uy uy"));
+  
+},[tareas])
+  useEffect(()=>{
+    if(!idForDelete) return;
+    let tareasTemporal = tareas.filter((tarea)=>{
+      console.log(idForDelete,tarea.id)
+     return idForDelete !== tarea.id
+    })
+    setTarea(tareasTemporal)
+  }, [idForDelete]);
+
   const handleSubmit = (event) => {
     if(!input.trim()) return;
     if (event.key === "Enter") {
       let objeto = {
+        label: '4Geeks',
+        done: false,
         id: (Math.random()*1000000).toFixed(3) ,
         texto: input,
       };
@@ -18,16 +45,6 @@ const TodoList = () => {
     }
   };
 
-  useEffect(()=>{
-    if(!idForDelete) return;
-    // console.log(`delete`,idForDelete)
-    let tareasTemporal = tareas.filter((tarea)=>{
-      console.log(idForDelete,tarea.id)
-     return idForDelete !== tarea.id
-    })
-
-    setTarea(tareasTemporal)
-  }, [idForDelete])
 
   return (
     <>
